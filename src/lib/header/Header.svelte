6f450fc162bd3ed3c7615c1d02db52f4751d40ce
“Lucian” <luciankallee@gmail.com>
2022-08-22 23:28:20 +0200

    updated navbar

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { page } from '$app/stores';
 	import Logo from "./LOGO.png";
+    let tab = "home";
+
 </script>
 
 <header>
@@ -17,17 +19,14 @@
 	  </button>
 	  <div class="hidden w-full md:block md:w-auto" id="navbar-default">
 		<ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-		  <li>
-			<a href="/" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+		  <li class="{tab === "home" ? 'border-orange-500 border-b-2 transition-all' : ''}">
+			<a href="/" on:click={() => tab = "home"} class="block py-2 pr-4 pl-3 text-gray-700 rounded md:bg-transparent md:p-0 dark:text-white" aria-current="page">Home</a>
 		  </li>
-		  <li>
-			<a href="/about" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+		  <li class="{tab === "about" ? 'border-orange-500 border-b-2 transition-all' : ''}">
+			<a href="/about" on:click={() => tab = "about"} class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400">About</a>
 		  </li>
-		  <li>
-			<a href="/marketplace" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Marketplace</a>
-		  </li>
-		  <li>
-			<a href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+		  <li class="{tab === "market" ? 'border-orange-500 border-b-2 transition-all' : ''}">
+			<a href="/marketplace" on:click={() => tab = "market"} class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Marketplace</a>
 		  </li>
 		  <li>
 			<a href="/" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
